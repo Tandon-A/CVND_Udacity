eb0e4a21c39ab9722723fc00c8db6d96435f26fa
Abhishek Tandon <abhishektnd@gmail.com>
2019-12-08 12:32:40 +0100

    Updated blur.cpp
    Added comments on optimizations.

--- optimized_code/blur.cpp
+++ optimized_code/blur.cpp
@@ -3,19 +3,9 @@
 
 using namespace std;
 
-// OPTIMIZATION: Pass large variable by reference
+// OPTIMIZATION: Large variables passed by reference
 vector < vector <float> > blur(vector < vector < float> > &grid, float blurring) {
 
-	// OPTIMIZATION: window, DX and  DY variables have the 
-    // same value each time the function is run.
-  	// It's very inefficient to recalculate the vectors
-    // every time the function runs. 
-    // 
-    // The const and/or static operator could be useful.
-  	// Define and declare window, DX, and DY using the
-    // bracket syntax: vector<int> foo = {1, 2, 3, 4} 
-    // instead of calculating these vectors with for loops 
-    // and push back
   	vector < vector <float> > newGrid;
 
 	int height;
@@ -29,23 +19,23 @@ vector < vector <float> > blur(vector < vector < float> > &grid, float blurring)
 	corner = blurring / 12.0;
 	adjacent = blurring / 6.0;
 
-  	
+	// OPTIMIZATION: Loop to initialize arrays removed.   	
 	vector < vector <float> > window = { {corner, adjacent, corner}, {adjacent, center, adjacent}, {corner, adjacent, corner} };
-
-
 	vector <int> DX = {-1, 0, 1};
 	vector <int> DY = {-1, 0, 1};  	
 
 
-	// OPTIMIZATION: Use your improved zeros function
+	// OPTIMIZATION: Using improved zeros function
 	newGrid = zeros(height,width);
   	int val, gval;
 	for (int i=0; i< height; i++ ) {
 		for (int j=0; j<width; j++ ) {
+		// OPTIMIZATION: Defining gval variable which will remain constant for below two for loops	
           	gval = grid[i][j];
 			for (int ii=0; ii<3; ii++) {
               	val = (i +  DY[ii] + height) % height;
 				for (int jj=0; jj<3; jj++) {
+					// OPTIMIZATION: Not storing values in different variables 
 					newGrid[val][((j + DX[jj] + width) % width)] += gval * window[ii][jj];
 				}
 			}
